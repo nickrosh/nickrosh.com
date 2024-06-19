727f7e943257675cd3bcbb837d897ec040d7cd3d
Nick Roshdieh <nickroshd@gmail.com>
2024-06-19 00:10:55 -0400

    f

--- data/projects.js
+++ data/projects.js
@@ -25,47 +25,6 @@ const items = [
       },
     ],
   },
-  {
-    year: '2021',
-    projects: [
-      {
-        title: 'Awesome Developer Experience',
-        url: 'https://github.com/workos-inc/awesome-developer-experience',
-        active: true,
-      },
-      {
-        title: 'ByteTalk',
-        url: 'https://bytetalkpodcast.com',
-        active: true,
-      },
-      {
-        title: 'Dracula UI',
-        url: 'https://draculatheme.com/ui',
-        active: true,
-      },
-    ],
-  },
-  {
-    year: '2020',
-    projects: [
-      {
-        title: '14 Habits',
-        description: 'Book about productivity',
-        url: 'https://14habits.com',
-        icon: 'book-2',
-        stats: '3k+ readers',
-        active: true,
-      },
-      {
-        title: 'Dracula PRO',
-        description: 'Premium dark color scheme',
-        url: 'https://draculatheme.com/pro',
-        icon: 'palette',
-        stats: '$210k+ sales',
-        active: true,
-      },
-    ],
-  },
 ]
 
 export default items
